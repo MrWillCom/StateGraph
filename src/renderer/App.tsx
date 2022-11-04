2633ca57c385759101218ca134bf51a2de2e0cba
Mr. Will <mr.will.com@outlook.com>
2022-11-04 13:07:18 +0800

    Add CPU section

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -7,7 +7,10 @@ import Win32WindowControls from './components/Win32WindowControls';
 
 class Home extends React.Component<
   {},
-  { ram: { used: number; total: number } }
+  {
+    ram: { used: number; total: number };
+    cpu: { model: string; used: number; total: number };
+  }
 > {
   constructor(props) {
     super(props);
@@ -17,17 +20,25 @@ class Home extends React.Component<
         used: 0,
         total: 1,
       },
+      cpu: {
+        model: 'Unknown',
+        used: 0,
+        total: 1,
+      },
     };
 
     this.ramInterval = 0;
+    this.cpuInterval = 0;
   }
 
   componentDidMount() {
     this.ramInterval = setInterval(this.updateRAM.bind(this), 1000);
+    this.cpuInterval = setInterval(this.updateCPU.bind(this), 1000);
   }
 
   componentWillUnmount() {
     clearInterval(this.ramInterval);
+    clearInterval(this.cpuInterval);
   }
 
   updateRAM() {
@@ -42,10 +53,66 @@ class Home extends React.Component<
     });
   }
 
+  updateCPU() {
+    const cpus = os.cpus();
+
+    let idle = 0;
+    let total = 0;
+    let models = [];
+
+    for (let i = 0, len = cpus.length; i < len; i++) {
+      const cpu = cpus[i];
+
+      total +=
+        cpu.times.user +
+        cpu.times.nice +
+        cpu.times.sys +
+        cpu.times.idle +
+        cpu.times.irq;
+      idle += cpu.times.idle;
+
+      if (
+        !models.find((val) => {
+          return val === cpu.model;
+        })
+      ) {
+        models.push(cpu.model);
+      }
+    }
+
+    this.setState({
+      cpu: {
+        model: (() => {
+          let modelsStr = '';
+
+          for (let i = 0; i < models.length; i++) {
+            modelsStr += models[i];
+            if (i < models.length - 1) {
+              modelsStr += ' & ';
+            }
+          }
+
+          return modelsStr;
+        })(),
+        used: total - idle,
+        total,
+      },
+    });
+  }
+
   render() {
     return (
       <>
         {navigator.platform === 'Win32' ? <Win32WindowControls /> : <></>}
+        <BarState
+          title="CPU"
+          description={this.state.cpu.model}
+          value={this.state.cpu.used}
+          total={this.state.cpu.total}
+          details={`${Math.round(
+            (this.state.cpu.used / this.state.cpu.total) * 100
+          )}%`}
+        />
         <BarState
           title="RAM"
           value={this.state.ram.used}
--- src/renderer/components/BarState.tsx
+++ src/renderer/components/BarState.tsx
@@ -2,17 +2,21 @@ import styles from './BarState.module.scss';
 
 type BarStateOptions = {
   title?: string;
+  description?: string;
   value: number;
   total: number;
   details?: string;
 };
 
 const BarState = (props: BarStateOptions) => {
-  const { title, value, total, details } = props;
+  const { title, description, value, total, details } = props;
   return (
     <div className={styles.container}>
       <div className={styles.trailing}>
-        <div className={styles.title}>{title || ''}</div>
+        <div className={styles.leftSide}>
+          <div className={styles.title}>{title || ''}</div>
+          <div className={styles.description}>{description || ''}</div>
+        </div>
         <div className={styles.details}>{details || `${value} / ${total}`}</div>
       </div>
       <div className={styles.bar}>
